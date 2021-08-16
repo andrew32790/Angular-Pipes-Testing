004a9071d33c56a2bc41e8b5a5bdac9961444641
Andrew <61470747+andrew32790@users.noreply.github.com>
2021-08-16 13:07:27 +0300

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit, ResolvedReflectiveFactory } from '@angular/core';
+import {Pipe, PipeTransform } from "@angular/core";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+@Pipe({
+  name: 'sort'
+})
+export class AppComponent implements PipeTransform{
+  transform(value:any, name:string):any{
+    return value.sort((a,b)=>{
+       if(a[name]>b[name]){
+         return 1;
+       }else{
+         return -1;
+       }
+    });
+ }
+
+  appStatus = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('stable');
+    }, 2000);
+  });
+  servers = [
+    {
+      instanceType: 'medium',
+      name: 'Production',
+      status: 'stable',
+      started: new Date(15, 1, 2017),
+    },
+    {
+      instanceType: 'large',
+      name: 'User Database',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Development Server',
+      status: 'offline',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Testing Environment Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    }
+  ];
+  filteredStatus = '';
+  getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
+    return {
+      'list-group-item-success': server.status === 'stable',
+      'list-group-item-warning': server.status === 'offline',
+      'list-group-item-danger': server.status === 'critical'
+    };
+  }
+  onAddServer() {
+    this.servers.push({
+      instanceType: 'small',
+      name: 'New Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    });
+    
+  }
+  
+
+ 
+   
+ 
+}
